9d4cf946e94d0f38a0498aa1927f4375834913f1
Sam Drew <sam-drew@outlook.com>
2018-11-07 21:06:19 +0000

    Create a Cards class for defining all things to do with Card objects.

---
+++ src/Cards.java
@@ -0,0 +1,12 @@
+public class Cards {
+    public class Card {
+        private int suitNumber;
+        private int valueNumber;
+
+        public Card(int suit, int value) {
+            suitNumber = suit;
+            valueNumber = value;
+        }
+    }
+
+}
